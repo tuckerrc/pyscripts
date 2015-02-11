f73b85ec05b2dcb05713e4f0a9026138c08116e1
Tucker Chapman <tucker.r.chapman@gmail.com>
2015-02-11 15:16:40 -0700

    Added color to div by location type
    River/Stream - Blue, Spring - green, Well - brown, Ditch - aqua, Reservoir - orange, Lake - darkblue, Atmosphere - skyblue

--- htmlwriter.py
+++ htmlwriter.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python 
-import csv, os, glob
+import csv, os, glob, re
 
 def stationReader(fN):
 	spans = []
@@ -8,7 +8,9 @@ def stationReader(fN):
 		i = 0
 		for row in reader:
 			if i != 0:
-				sp = "<div>"
+				htmlClass = row[4]
+				htmlClass = re.sub('[^0-9a-zA-Z]+', ' ', htmlClass)
+				sp = "<div class='" + htmlClass + "'>"
 				sp = sp+"<h1>"+row[2]+"</h1>"
 				sp = sp+"<strong>Organization: </strong> "+row[1]+"<br/>"
 				sp = sp+"<strong>Location Name: </strong> "+row[3]+"<br/>"
@@ -20,7 +22,7 @@ def stationReader(fN):
 	f.close()	  	
 	return spans
 def htmlWriter(array):
-	start = "<html><head><style>html,body{height:100%;width: 100%;margin: 0px;}div{display:inline-block;padding:30px;width:300px;margin:10px;border:2px solid black;} h1{margin:0px;font-size:18px;}</style></head><body>"
+	start = "<html><head><style>html,body{height:100%;width: 100%;margin: 0px;}div{display:inline-block;padding:30px;width:300px;margin:10px;border:2px solid black; overflow:hidden;} h1{margin:0px;font-size:18px;} .Stream,.River.Stream{border-color: blue;} .Spring{border-color:green;}.Well{border-color:brown;} .Ditch{border-color:aqua;} .Lake.Reservoir{border-color:orange;} .Lake{border-color:darkblue;}.Atmosphere{border-color:skyblue;}</style></head><body>"
 	end = "</body></html>"
 	string = ""
 	for i in array:
